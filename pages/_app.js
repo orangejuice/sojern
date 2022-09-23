b3211cc1c3306e7c1a7af536ca42bd07729cf389
orangejuice <cherrymeteor@outlook.com>
2022-09-23 15:10:45 +0100

    fix ackee

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import '../styles/global.css'
 import {useEffect, useState} from "react";
-import * as ackeeTracker from 'ackee-tracker'
-import {Router} from "next/router";
 
 function MyApp({Component, pageProps}) {
   let dogs = []
@@ -11,18 +9,11 @@ function MyApp({Component, pageProps}) {
   const [likedDogs, setLikedDogs] = useState(dogs)
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      window.ackeeTrackerInstance = ackeeTracker.create({
-        server: 'https://ackee.orangejuice.vercel.app',
-        domainId: 'a398f2e6-2c27-481c-9fdf-a7ce13d3a047'
-      }, {
-        ignoreLocalhost: true,
-        detailed: true
-      })
-      window.ackeeTrackerInstance.record()
-
-      Router.events.on('routeChangeComplete', window.ackeeTrackerInstance.record())
-    }
+    const ackeeTracker = require('ackee-tracker')
+    ackeeTracker.create('https://ackee.orangejuice.vercel.app', {
+      ignoreLocalhost: true,
+      detailed: true
+    }).record('a398f2e6-2c27-481c-9fdf-a7ce13d3a047')
   }, [])
 
   return <Component {...pageProps} likedDogs={likedDogs} setLikedDogs={setLikedDogs}/>
