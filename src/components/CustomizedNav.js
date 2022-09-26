3b7a5321e5e19609fb22008a96520af41904bd69
orangejuice <cherrymeteor@outlook.com>
2022-09-26 14:00:37 +0100

    Nav highlight current page

--- src/components/CustomizedNav.js
+++ src/components/CustomizedNav.js
@@ -8,17 +8,17 @@ export default function CustomizedNav(props) {
     <>
       <h1 className={styles.title}>Favorite dogs</h1>
       <nav className={styles.description + ' ' + styles.nav}>
-        {/*<Link href='/'>*/}
-        {/*  <a className={home ? styles.current : ''}>Home</a>*/}
-        {/*</Link>*/}
+        <Link href='/'>
+          <a className={home ? styles.current : ''}>Home</a>
+        </Link>
 
-        <Link href='/'>Home</Link>
+        {/*<Link href='/'>Home</Link>*/}
         <span style={{width: 100, display: 'inline-block', height: 10}}></span>
-        <Link href='/favorites'>Favorites</Link>
+        {/*<Link href='/favorites'>Favorites</Link>*/}
 
-        {/*<Link href='/favorites'>*/}
-        {/*  <a className={home ? '' : styles.current}>Favorites</a>*/}
-        {/*</Link>*/}
+        <Link href='/favorites'>
+          <a className={home ? '' : styles.current}>Favorites</a>
+        </Link>
       </nav>
     </>
   )
