6cc50baf734b97f1a3eb61e62bc5c4b43627f911
orangejuice <cherrymeteor@outlook.com>
2022-09-26 12:10:06 +0100

    fix nav

--- src/components/CustomizedNav.js
+++ src/components/CustomizedNav.js
@@ -5,9 +5,9 @@ export default function CustomizedNav({title}) {
   return <>
     <h1 className={styles.title}>{title}</h1>
     <nav className={styles.description + ' ' + styles.nav}>
-      <Link href='/src/pages'>Home</Link>
+      <Link href='/'>Home</Link>
       <span style={{width: 100, display: 'inline-block', height: 10}}></span>
-      <Link href='src/pages/favorites'>Favorites</Link>
+      <Link href='/favorites'>Favorites</Link>
     </nav>
   </>;
 }
